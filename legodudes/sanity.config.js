640952e1ead696a9fa36e5be76dacd04a81fbffb
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-03-19 14:27:06 +0100

    sanity oppsetting

---
+++ legodudes/sanity.config.js
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'LEGODUDES',
+
+  projectId: 'kqf7yid1',
+  dataset: 'production',
+
+  plugins: [
+    structureTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
