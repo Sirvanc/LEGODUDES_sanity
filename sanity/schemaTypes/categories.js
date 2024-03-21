271dff6df60b0c330cf2af5fe5a7ba65891a79db
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-03-21 12:55:43 +0100

    oppdatert

--- sanity/schemaTypes/categories.js
+++ sanity/schemaTypes/categories.js
@@ -2,26 +2,20 @@ export const categories = {
     title: "Kategorier",
     name: "categories",
     type: "document",
-
-
     fields: [
         {
-         title: "Kategorier",
-         name: "Categorytitle",
-         type: "string"
-
- },
- {
-    title:"Kategorilink",
-    name: "Categoryurl",
-    type: "slug",
-    options:{
-        source:"Categorytitle",
-        slugify: input => input.toLowerCase().replace(/\s+/g, '-')
-    },
-
- }
- ]
-
-
+            title: "Kategoritittel",
+            name: "categorytitle",
+            type: "string"
+        },
+        {
+            title: "Kategorilink",
+            name: "categoryurl",
+            type: "slug",
+            options: {
+                source: "categorytitle",
+                slugify: input => input.toLowerCase().replace(/\s+/g, '-')
+            }
+        }
+    ]
 }
